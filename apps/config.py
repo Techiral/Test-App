d0c027cdb91637743c8fe71e97f8c6045eaf66b4
Techiral <115097129+Techiral@users.noreply.github.com>
2022-10-21 11:37:24 -0700

    Update config.py

--- apps/config.py
+++ apps/config.py
@@ -9,7 +9,7 @@ class Config(object):
 
     basedir = os.path.abspath(os.path.dirname(__file__))
     DEBUG = False   
-    OPENAI_API_KEY= os.getenv('OPENAI_API_KEY', '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')
+    OPENAI_API_KEY= os.getenv('OPENAI_API_KEY', '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')
     ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
     FLASK_APP= "app"
     FLASK_ENV= "development"
